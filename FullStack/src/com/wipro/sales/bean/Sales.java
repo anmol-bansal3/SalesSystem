c1661c3c234192a435400993cfd982f1593622d0
Anmol <bansalanmol41.ab@gmail.com>
2020-11-02 10:52:11 +0530

    bean class made

---
+++ FullStack/src/com/wipro/sales/bean/Sales.java
@@ -0,0 +1,33 @@
+package com.wipro.sales.bean;
+
+public class Sales {
+	private String salesID, productID;
+	java.util.Date salesDate;
+	private int quantitySold;
+	private double salesPricePerUnit;
+	public String getSalesID() {
+		return salesID;
+	}
+	public void setSalesID(String salesID) {
+		this.salesID = salesID;
+	}
+	public String getProductID() {
+		return productID;
+	}
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+	public int getQuantitySold() {
+		return quantitySold;
+	}
+	public void setQuantitySold(int quantitySold) {
+		this.quantitySold = quantitySold;
+	}
+	public double getSalesPricePerUnit() {
+		return salesPricePerUnit;
+	}
+	public void setSalesPricePerUnit(double salesPricePerUnit) {
+		this.salesPricePerUnit = salesPricePerUnit;
+	}
+	
+}
---
+++ FullStack/src/com/wipro/sales/bean/SalesReport.java
@@ -0,0 +1,50 @@
+package com.wipro.sales.bean;
+
+public class SalesReport {
+	private String salesID, productID, productName;
+	java.util.Date salesDate;
+	private int quantitySold;
+	private double productUnitPrice, salesPricePerUnit, profitAmount;
+	public String getSalesID() {
+		return salesID;
+	}
+	public void setSalesID(String salesID) {
+		this.salesID = salesID;
+	}
+	public String getProductID() {
+		return productID;
+	}
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getQuantitySold() {
+		return quantitySold;
+	}
+	public void setQuantitySold(int quantitySold) {
+		this.quantitySold = quantitySold;
+	}
+	public double getProductUnitPrice() {
+		return productUnitPrice;
+	}
+	public void setProductUnitPrice(double productUnitPrice) {
+		this.productUnitPrice = productUnitPrice;
+	}
+	public double getSalesPricePerUnit() {
+		return salesPricePerUnit;
+	}
+	public void setSalesPricePerUnit(double salesPricePerUnit) {
+		this.salesPricePerUnit = salesPricePerUnit;
+	}
+	public double getProfitAmount() {
+		return profitAmount;
+	}
+	public void setProfitAmount(double profitAmount) {
+		this.profitAmount = profitAmount;
+	}
+}
