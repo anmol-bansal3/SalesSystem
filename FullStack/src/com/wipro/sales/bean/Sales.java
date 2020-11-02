60cf70949412ffa707f4b0f6376a12e1f94ac4e1
Anmol <bansalanmol41.ab@gmail.com>
2020-11-02 11:39:01 +0530

    made a change_log having database and add static method tosales.java

--- FullStack/src/com/wipro/sales/bean/Sales.java
+++ FullStack/src/com/wipro/sales/bean/Sales.java
@@ -2,33 +2,39 @@ package com.wipro.sales.bean;
 import java.util.*;
 
 public class Sales {
-	private String salesID, productID;
-	java.util.Date salesDate;
-	private int quantitySold;
+	private static String salesID, productID;
+	java.util.Date date = new java.util.Date();
+	java.sql.Date salesDate = new java.sql.Date(date.getTime());
+	
+	private static int quantitySold;
 	private double salesPricePerUnit;
-	public String getSalesID() {
+	public static String getSalesID() {
 		return salesID;
 	}
 	public void setSalesID(String salesID) {
 		this.salesID = salesID;
 	}
-	public String getProductID() {
+	public static String getProductID() {
 		return productID;
 	}
 	public void setProductID(String productID) {
 		this.productID = productID;
 	}
-	public int getQuantitySold() {
+	public static int getQuantitySold() {
 		return quantitySold;
 	}
 	public void setQuantitySold(int quantitySold) {
 		this.quantitySold = quantitySold;
 	}
-	public double getSalesPricePerUnit() {
+	public static double getSalesPricePerUnit() {
 		return salesPricePerUnit;
 	}
 	public void setSalesPricePerUnit(double salesPricePerUnit) {
 		this.salesPricePerUnit = salesPricePerUnit;
+	}
+	public static String salesDate() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
--- FullStack/src/com/wipro/sales/dao/SalesDao.java
+++ FullStack/src/com/wipro/sales/dao/SalesDao.java
@@ -1,13 +1,34 @@
 package com.wipro.sales.dao;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.*;
+import java.text.*;
+import com.wipro.sales.bean.Sales;
+import com.wipro.sales.util.DBUtil;
+
 
 public class SalesDao {
-	public int insertSales(Sales sales) {
-		
+	public int insertSales(Sales sales) throws Exception{
+		Connection con = DBUtil.getDBConnection();
+		Statement st = con.createStatement();
+		String record = "INSERT INTO `TBL_SALES`(`salesID`, `productID`, `salesDate`, `quantitySold`, `salesPricePerUnit`)" + "VALUES ('"+Sales.getSalesID()+"','"+Sales.getProductID()+"','"+Sales.salesDate()+"','"+Sales.getQuantitySold()+"','"+Sales.getSalesPricePerUnit()+"')";
+		if(st.executeUpdate(record) == 1) {
+			return 1;
+		}
+		return 0;
 	}
-	public String generateSalesID(java.util.Date salesDate) {
-		return null;
+	public String generateSalesID(java.util.Date salesDate) throws Exception{
+		DateFormat df = new SimpleDateFormat("yy");
+	    String formattedDate = df.format(Calendar.getInstance().getTime());
+	    
+	    Connection con = DBUtil.getDBConnection();
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT `id` FROM `TBL_SALES`");
 		
+		String salesId = formattedDate + rs.getInt("id");
+		
+		return salesId;	
 	}
 	public Arraylist<SalesReport> getSalesReport() { 
 		
